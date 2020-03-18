29b34c4199733a1cab8071152d858c26557c257f
Kent Paraiso <kent.paraiso@gmail.com>
2020-03-18 19:17:26 +0800

    Updates README.md, formatting, and comments out node3.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # k3os-terraform-libvirt
+
+This terraform module helps me set up my home Kubernetes lab based on [Rancher's k3os](https://github.com/rancher/k3os)
+
+## Prerequisites
+
+- A working libvirt and qemu stack
+- terraform >= 0.12
+- [terraform-provider-libvirt](https://github.com/dmacvicar/terraform-provider-libvirt)
+
+## Miscellaneous
+
+Your libvirt's [access control](https://libvirt.org/aclpolkit.html) might prompt you for a password everytime you
+fire up a build. You might want to follow the directions mentioned [here](https://www.poftut.com/use-virt-manager-libvirt-normal-user-without-root-privileges-without-asking-password/).
+
+## Quickstart
+
+1. Download k3os-vmlinuz-amd64, k3os-initrd-amd64, and k3os-amd64.iso, place those images in ./template/images
+2. cd to ```./template```
+3. Customize ```main.tf``` according to your requirements.
+4. Execute ```terraform plan```
+5. If you like what you see. Run ```terraform apply```.
--- templates/main.tf
+++ templates/main.tf
@@ -14,34 +14,34 @@ module "k3os_server" {
 }
 
 module "k3os_agent1" {
-  source = "../modules/k3os-instance"
-  hostname = "k3os-agent1"
-  role = "agent"
+  source            = "../modules/k3os-instance"
+  hostname          = "k3os-agent1"
+  role              = "agent"
   storage_pool_name = module.k3os_storage_pool.pool_name
-  server_url = "https://${module.k3os_server.hostname}:6443"
+  server_url        = "https://${module.k3os_server.hostname}:6443"
   labels = {
-    "plan.upgrade.cattle.io/k3os-latest": "enabled"
+    "plan.upgrade.cattle.io/k3os-latest" : "enabled"
   }
 }
 
 module "k3os_agent2" {
-  source = "../modules/k3os-instance"
-  hostname = "k3os-agent2"
-  role = "agent"
+  source            = "../modules/k3os-instance"
+  hostname          = "k3os-agent2"
+  role              = "agent"
   storage_pool_name = module.k3os_storage_pool.pool_name
-  server_url = "https://${module.k3os_server.hostname}:6443"
+  server_url        = "https://${module.k3os_server.hostname}:6443"
   labels = {
-    "plan.upgrade.cattle.io/k3os-latest": "enabled"
+    "plan.upgrade.cattle.io/k3os-latest" : "enabled"
   }
 }
 
-module "k3os_agent3" {
-  source = "../modules/k3os-instance"
-  hostname = "k3os-agent3"
-  role = "agent"
-  storage_pool_name = module.k3os_storage_pool.pool_name
-  server_url = "https://${module.k3os_server.hostname}:6443"
-  labels = {
-    "plan.upgrade.cattle.io/k3os-latest": "enabled"
-  }
-}
+# module "k3os_agent3" {
+#   source = "../modules/k3os-instance"
+#   hostname = "k3os-agent3"
+#   role = "agent"
+#   storage_pool_name = module.k3os_storage_pool.pool_name
+#   server_url = "https://${module.k3os_server.hostname}:6443"
+#   labels = {
+#     "plan.upgrade.cattle.io/k3os-latest": "enabled"
+#   }
+# }
